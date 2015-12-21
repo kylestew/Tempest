cd96327893240f6b501262d7c2c0b2cf92626097
Kyle Stewart <kylestew@gmail.com>
2015-12-20 22:24:42 -0800

    Photon now able to control thermostat, time to build an app for it

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -1,189 +1,61 @@
 #include "HTU21D.h"
 
+#define LED_INFO     D7
+#define IR_OUT       D2
+
 HTU21D htu = HTU21D();
+
+double humd = 0;
+double temp = 0;
 
 void setup() {
 	Serial.begin(9600);
-	Serial.println("HTU21D test");
 
+  pinMode(LED_INFO, OUTPUT);
+  pinMode(IR_OUT, OUTPUT);
+
+	// connect to I2C temp
 	while(! htu.begin()){
 	    Serial.println("HTU21D not found");
 	    delay(1000);
 	}
 
-	Serial.println("HTU21D OK");
+	// track variables
+	Particle.variable("humd", humd);
+	Particle.variable("temp", temp);
+
+	// setup IR function
+	Particle.function("writeTo", updateThermostat);
 }
 
 void loop() {
-	Serial.println("===================");
-	Serial.print("Hum:"); Serial.println(htu.readHumidity());
-	Serial.print("Temp:"); Serial.println(htu.readTemperature());
-	Serial.println();
-
+	// poll current temp values
+	humd = htu.readHumidity();
+	temp = htu.readTemperature();
 	delay(1000);
 }
 
+int updateThermostat(String command) {
+	// TODO: how to get useful data from this command
+	/* FORMAT:
+	  TEMP|FANSPEED
+		4201 = 42c / 1 (slow)
+	*/
 
-/*
-#include <ESP8266WiFi.h>
-#include <Wire.h>
-#include <SparkFunHTU21D.h>
+	Serial.println(command);
 
-#define LED_INFO     5
-#define IR_OUT       4
+	// temp: 64-88deg (will cap)
+	// fanSpeed: 0 - auto, 1 - high, 2 - med, 3 - low, 4 - quiet
+	double temp = 88;
+	double fanSpeed = 1;
+  updateAC(temp, fanSpeed);
 
-
-WiFiClient client;
-HTU21D humidity;
-
-unsigned long connectionRequestStartTime = 0; // last time you connected to the server, in milliseconds
-const unsigned long connectionRequestTimeout = 10L * 1000L; // delay between updates, in milliseconds
-
-void setup() {
-  Serial.begin(9600);
-
-  // LED INFO
-  pinMode(LED_INFO, OUTPUT);
-  digitalWrite(LED_INFO, LOW);
-
-  // IR LED
-  pinMode(IR_OUT, OUTPUT);
-
-  humidity.begin();
-
-  // boot up wifi
-  byte ledStatus = LOW;
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA); // connect to local wifi, don't make an access point
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    // Blink the LED
-    digitalWrite(LED_INFO, ledStatus); // Write LED high/low
-    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
-
-    // blocking loops will cause SOC to reset
-    delay(100);
-    Serial.print(".");
-  }
-  digitalWrite(LED_INFO, HIGH);
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	return 0;
 }
 
-void loop()  {
-  digitalWrite(LED_INFO, HIGH);
-
-  // POLL for AC settings change
-  if (client.connect(serverUrl, PORT)) {
-    // start request for settings
-    client.println("GET /settings HTTP/1.1");
-    client.println("Accept-Encoding: gzip, deflate");
-    client.println("Connection: keep-alive");
-    client.println("Host: temply.meteor.com:80");
-    client.println();
-    connectionRequestStartTime = millis();
-    Serial.println("settings request sent");
-
-    // poll client for response
-    bool finished = false;
-    while (!finished) {
-      bool recording = false;
-      byte newTemp = 0;
-      byte newFanSpeed = 0;
-      while (client.available()) {
-        char c = client.read();
-        Serial.print(c);
-
-        if (c == '*' && recording) {
-          recording = false;
-          finished = true;
-          continue; // allow client to consume remaining HTTP response
-        }
-
-        // server needs to send 4 byte data block = *AB* (A = temp, B = fanSpeed)
-        if (recording) {
-          if (newTemp == 0)
-            newTemp = c;
-          else
-            newFanSpeed = c;
-        }
-
-        if (c == '*' && !recording) {
-          recording = true;
-        }
-      }
-
-      if (finished) {
-        changeACSettings(newTemp, newFanSpeed);
-        break;
-      }
-
-      delay(100);
-
-      if (millis() - connectionRequestStartTime > connectionRequestTimeout) {
-        Serial.println("response timed out");
-        break; // timeout response handling
-      }
-    }
-    client.stop();
-  } else {
-    digitalWrite(LED_INFO, LOW);
-  }
-
-  delay(6000); // seperate POLL and PUSH
-
-  if (client.connect(serverUrl, PORT)) {
-    // read values
-    float humd = humidity.readHumidity();
-    float temp = humidity.readTemperature();
-    temp = temp * (9/5.0) + 32;
-
-    Serial.print("temp: ");
-    Serial.println(temp, 1);
-    Serial.print("humid: ");
-    Serial.println(humd, 1);
-
-    client.print("PUT /record?temp=");
-    client.print(temp, 1);
-    client.print("&humd=");
-    client.print(humd, 1);
-    client.println(" HTTP/1.1");
-    client.println("Accept-Encoding: gzip, deflate");
-    client.println("Connection: keep-alive");
-    client.println("Content-Length: 0");
-    client.println("Host: temply.meteor.com:80");
-    client.println();
-  } else {
-    digitalWrite(LED_INFO, LOW);
-  }
-
-  delay(6000); // seperate POLL and PUSH
-}
-*/
 /*==== AC INTERFACE ====*/
-/*
-// default AC status
-int temp = 0;
-int fanSpeed = 0;
-
-void changeACSettings(byte newTemp, byte newFanSpeed) {
-  Serial.print("New values: ");
-  Serial.print(newTemp, DEC);
-  Serial.print(" : ");
-  Serial.println(newFanSpeed, DEC);
-
-  if (newTemp != temp || newFanSpeed != fanSpeed) {
-    temp = newTemp;
-    fanSpeed = newFanSpeed;
-    updateAC(temp, fanSpeed);
-  }
-}
-
 void pulseIR(long usecs) {
-  cli();  // disable interrupts
+	noInterrupts(); // disable interrupts
 
   while (usecs > 0) {
     // 38 kHz is about 13 microseconds high and 13 microseconds low
@@ -196,7 +68,7 @@ void pulseIR(long usecs) {
    usecs -= 26;
   }
 
-  sei();
+	interrupts();
 }
 
 void sendIRByte(byte bite) {
@@ -207,7 +79,8 @@ void sendIRByte(byte bite) {
 
     // 0: 46 cycles OFF * 26.3 = 1200
     // 1: 16 cycles OFF * 26.3 = 420
-    uint16_t usec = bite & _BV(i) ? 1200 : 420;
+    uint16_t usec = bite & 0x00000001 ? 1200 : 420;
+		bite = bite >> 1;
     delayMicroseconds(usec);
   }
 }
@@ -272,4 +145,3 @@ void updateAC(byte temp, byte fanSpeed) {
   pulseIR(420); // 16 * 26.3 = 420
   delayMicroseconds(8000); // 305 * 26.3 = ~8000
 }
-*/
