b726a7c42805df3c95a4d0bda9ce7ac594b9a6f8
Kyle Stewart <kylestew@gmail.com>
2015-12-21 18:11:02 -0800

    pods

---
+++ Tempest/Pods/Target Support Files/Pods-TempestTests/Pods-TempestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TempestTests : NSObject
+@end
+@implementation PodsDummy_Pods_TempestTests
+@end
---
+++ Tempest/Pods/Target Support Files/Pods-TempestUITests/Pods-TempestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TempestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TempestUITests
+@end
