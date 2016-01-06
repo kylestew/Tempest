59c40dab355039d00f22ff899b42340818a746f6
Kyle Stewart <kylestew@gmail.com>
2016-01-05 21:57:07 -0800

    Fan speed UI working, need to finish temperature select and display dial

--- Tempest/Tempest/ThermostatViewController.swift
+++ Tempest/Tempest/ThermostatViewController.swift
@@ -10,6 +10,7 @@ class ThermostatViewController: UIViewController, AuthDelegate, ThermostatDelega
     @IBOutlet weak var fan1Button: UIButton!
     @IBOutlet weak var fan2Button: UIButton!
     @IBOutlet weak var fan3Button: UIButton!
+    var fanButtons:[UIButton]!
     
 //    @IBOutlet weak var actualTempLabel: UILabel!
 //    @IBOutlet weak var targetTempLabel: UILabel!
@@ -27,6 +28,9 @@ class ThermostatViewController: UIViewController, AuthDelegate, ThermostatDelega
         fanAutoButton.layer.borderWidth = 2.0
         fanAutoButton.layer.cornerRadius = 2.0
         
+        // easy access
+        fanButtons = [ fan0Button, fan1Button, fan2Button, fan3Button ]
+        
         
 //        thermostat.temperature.producer.startWithNext { (temperature) -> () in
 //            self.actualTempLabel.text = String(format: "%0.0f", temperature)
@@ -34,31 +38,42 @@ class ThermostatViewController: UIViewController, AuthDelegate, ThermostatDelega
 //        thermostat.targetTemp.producer.startWithNext { (target) -> () in
 //            self.targetTempLabel.text = String(format: "%d", target)
 //        }
+        
         thermostat.masterControl.producer.startWithNext { mode in
             self.masterControlButton.setTitle(mode.displayName().uppercaseString, forState: .Normal)
             switch (mode) {
             case .Cool:
                 self.view.backgroundColor = UIColor(hex: 0x71a5f7)
+                self.fanAutoButton.backgroundColor = UIColor(hex: 0x3989FC)
             case .Heat:
                 self.view.backgroundColor = UIColor(hex: 0xfe9964)
+                self.fanAutoButton.backgroundColor = UIColor(hex: 0xfa7036)
             case .Off:
                 self.view.backgroundColor = UIColor(hex: 0xadadad)
             }
+            
+            // disable all fan speed buttons if master control is off
+            let isOff = mode == .Off
+            self.fanAutoButton.hidden = isOff
+            for button in self.fanButtons { button.enabled = !isOff }
         }
         thermostat.fanSpeed.producer.startWithNext { fanSpeed in
-//            self.fanSpeedSegmentedControl.selectedSegmentIndex = fanSpeed.rawValue
-            
             if (fanSpeed == .Auto) {
                 self.fanAutoButton.layer.borderWidth = 2.0
-                self.fanAutoButton.enabled = true
-                
+                self.fanAutoButton.alpha = 1.0
+                for button in self.fanButtons { button.alpha = 0.5 }
             } else {
                 self.fanAutoButton.layer.borderWidth = 0
-                self.fanAutoButton.enabled = false
-                
+                self.fanAutoButton.alpha = 0.5
+                var i = 1
+                for button in self.fanButtons {
+                    if i++ > FanSpeeds.Quiet.rawValue - fanSpeed.rawValue + 1 {
+                        button.alpha = 0.5
+                    } else {
+                        button.alpha = 1.0
+                    }
+                }
             }
-            
-            print("FAN: \(fanSpeed)")
         }
     }
 
